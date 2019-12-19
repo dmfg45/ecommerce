729d1f0e3a9c0bf5713b43ba036da880c8dc2952
André Graça <andre.graca.45@gmail.com>
2019-12-19 01:57:17 +0000

    updating project

---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace  Hcode;
+use Rain\Tpl;
+
+class Page
+{
+
+    private $tpl;
+    private $options = [];
+    private $defaults = [
+        "data"=>[]
+    ];
+
+    public function __construct($options = array())
+    {
+        $this->options = array_merge($this->defaults, $options);
+
+        // config
+        $config = array(
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+            "debug"         => false // set to false to improve the speed
+        );
+
+        Tpl::configure( $config );
+
+        $this->tpl = new Tpl();
+
+        $this->setData($this->options["data"]);
+
+        $this->tpl->draw("header");
+
+    }
+
+    private function setData($data = array()){
+        foreach ($data as $key => $value){
+            $this->tpl->assign($key, $value);
+        }
+    }
+
+    public function setTpl($templateName, $data = array(), $returnHtml = false){
+
+        $this->setData($data);
+       return $this->tpl->draw($templateName, $returnHtml);
+
+    }
+
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+        $this->tpl->draw("footer");
+    }
+
+}
